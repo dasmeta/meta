75fbb0a04c972bbe4d71cd7dab5927dad9eac762
mrdntgrn <31103575+mrdntgrn@users.noreply.github.com>
2022-07-25 11:50:48 +0400

    Have wget save faile to destinaton path and chmod

--- scripts/add-this-linux.sh
+++ scripts/add-this-linux.sh
@@ -3,14 +3,12 @@
 while true; do
     read -p "First we have to install Meta, is that okay? Y or N? If you choose N this script will exit." yn
     case $yn in
-        [Yy]* ) /bin/bash -c "$(wget https://github.com/dasmeta/meta/releases/download/v0.1.0/meta-linux)"; break;;
+        [Yy]* ) sudo wget https://github.com/dasmeta/meta/releases/download/v0.1.0/meta-linux -O /usr/bin/meta; sudo chmod +x /usr/bin/meta
+; break;;
         [Nn]* ) echo Exiting!; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-mv meta-linux meta
-cp meta /usr/bin
-chmod +x /usr/bin/meta
 echo All done!
 exit
