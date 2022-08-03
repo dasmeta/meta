4e98f4b488353e45509a12eea5062435a5b43772
0katrinpetrosyan0 <katrinpetrosyan321@gmail.com>
2022-08-03 17:27:30 +0400

    DMVP-1029 have some fixes

--- scripts/add-this-linux.sh
+++ scripts/add-this-linux.sh
@@ -3,15 +3,11 @@
 while true; do
     read -p "First we have to install Meta, is that okay? Y or N? If you choose N this script will exit." yn
     case $yn in
-        [Yy]* ) sudo wget https://github.com/dasmeta/meta/releases/download/v0.1.0/meta-linux -O /usr/bin/meta; sudo chmod +x /usr/bin/meta
-; break;;
+        [Yy]* ) sudo wget https://github.com/dasmeta/meta/releases/download/v0.1.0/meta-linux -O /usr/local/bin/meta; sudo chmod +x /usr/local/bin/meta; break;;
         [Nn]* ) echo Exiting!; exit;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
-sudo mv meta-linux meta
-sudo cp meta /usr/local/bin/
-sudo chmod +x /usr/local/bin/meta
 echo All done!
 exit
